d4f06916eefa6c73fe2ac96041e0a12a50351fd2
HiroIshida <spitfire.docomo@gmail.com>
2020-04-03 03:10:30 +0900

    convert pc to custom message

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,17 @@
 #include <geometry_msgs/Point32.h>
 #include <pcl_conversions/pcl_conversions.h> // japanese version
 
+#include <object_observer/Cloud.h>
+
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
-#include "geometry_msgs/Point.h" 
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
+
+#include <geometry_msgs/Point32.h>
 
 void callback (const sensor_msgs::PointCloud2ConstPtr& msg_input)
 {
@@ -22,21 +25,21 @@ void callback (const sensor_msgs::PointCloud2ConstPtr& msg_input)
   // tutorial in Japanese is wrong (using depricated header)  
   pcl::PointCloud<pcl::PointXYZ> cloud;
   pcl::fromROSMsg(*msg_input, cloud); 
-  std::vector<std::vector<float>> vec_cloud;
-  for(int i=0; i< cloud.points.size(); i++){
-    std::vector<float> vec{ 
-      cloud.points[i].x,
-      cloud.points[i].y,
-      cloud.points[i].z};
-    vec_cloud.push_back(vec);
-  }
 
+  std_msgs::Float32MultiArray x_array, y_array, z_array;
+  for(int i=0; i< cloud.points.size(); i++){
+    x_array.data[i] = cloud.points[i].x;
+    y_array.data[i] = cloud.points[i].y;
+    z_array.data[i] = cloud.points[i].z;
+  }
+  object_observer::Cloud msg;
+  msg.x_array = x_array;
+  msg.y_array = y_array;
+  msg.z_array = z_array;
 }
 
 int main (int argc, char** argv)
 {
-  tmp();
-  int i = 1;
   ros::init(argc, argv, "projector");
   ros::NodeHandle nh;
   ros::Subscriber sub = nh.subscribe("/core/multi_plane_extraction/output_nonplane_cloud", 10, callback);
--- src/process.cpp
+++ src/process.cpp
@@ -3,7 +3,6 @@ void tmp(){
   std::cout<<"fuck"<<std::endl;
 }
 
-void process_vec_cloud(std::vector<std::vector<float>> vec_cloud)
+void process_vec_cloud(std::vector<std::vector<float>>& vec_cloud)
 {
-  std::cout<<"fuck1"<<std::endl;
 }
