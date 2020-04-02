ace51e8364bb24516dd62eeccbe60ef5cf4eb209
HiroIshida <spitfire.docomo@gmail.com>
2020-04-03 02:58:26 +0900

    add testers

--- src/process.cpp
+++ src/process.cpp
@@ -2,3 +2,8 @@
 void tmp(){
   std::cout<<"fuck"<<std::endl;
 }
+
+void process_vec_cloud(std::vector<std::vector<float>> vec_cloud)
+{
+  std::cout<<"fuck1"<<std::endl;
+}
--- src/process.hpp
+++ src/process.hpp
@@ -1,3 +1,5 @@
 #include<iostream>
+#include<vector>
 void tmp();
+void process_vec_cloud(std::vector<std::vector<float>> vec_cloud);
 
